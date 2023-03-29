d65b01a21196ab687c2075e9c56c28bba78a5efb
Itamarush <itamar.guetta@gmail.com>
2023-03-29 20:23:29 +0300

    Final Commit

--- Test.cpp
+++ Test.cpp
@@ -11,15 +11,18 @@ TEST_CASE("Checking if the given name is actually the name")
 {
     Player player1("Itamar");
     Player player2("Guetta");
-    CHECK(player1.name == "Itamara");
+    CHECK(player1.name == "Itamar");
     CHECK(player2.name == "Guetta");
+    CHECK(player1.name != player2.name);
 }
 
 TEST_CASE("Checking if the intiallization of the player is good")
 {
-    Player player1("ItamarG");
+    Player player1("Itamar");
+    Player player2("Guetta");
     CHECK(player1.stacksize() == 0);
     CHECK(player1.cardesTaken() == 0);
+    CHECK(player1.stacksize() == player2.stacksize());
 }
 
 TEST_CASE("Checking if we starts the game with 52 cards")
@@ -50,5 +53,18 @@ TEST_CASE("checks if the cards are well set")
     CHECK(card1.getKind() == card1.getKind());
     CHECK(card1.getKind() == card2.getKind());
     CHECK(card1.getKind() != card3.getKind());
+}
+
+TEST_CASE("checks if its throws any excepetions")
+{
+    Player player1("Itamar");
+    Player player2("Guetta");
+    Game game(player1, player2);
+    CHECK_THROWS(game.playAll());
+    CHECK_THROWS(game.printWiner());
+    CHECK_THROWS(game.printLog());
+    CHECK_THROWS(game.printLastTurn());
+    CHECK_THROWS(game.playTurn());
+    CHECK_THROWS(game.printStats());
 }
 
